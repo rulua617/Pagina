814b2a9961335829cd1305cb6d6f3f6ccee82f7a
Jhojan Aguilar <jhojan.aguilar.13.10.04@gmail.com>
2023-11-09 21:39:05 -0500

    pV4.5

--- scripts/poup.js
+++ scripts/poup.js
@@ -30,3 +30,46 @@ function mostrarPopUp(texto) {
 function cerrarPopUp() {
     popUp.style.display = 'none';
 }
+
+
+/* barra de busqueda */
+
+    document.getElementById('search').addEventListener('input', function () {
+        var searchTerm = this.value.toLowerCase(); 
+        var contenedores = document.querySelectorAll('.contenedor');
+            contenedores.forEach(function (contenedor) {
+            var elementosBoton = contenedor.querySelectorAll('.boton'); 
+                var elementosFiltrados = Array.from(elementosBoton).filter(function (elemento) {
+                return elemento.textContent.toLowerCase().includes(searchTerm);
+            });
+    
+            var orden = 0;
+            elementosBoton.forEach(function (elemento) {
+                if (elementosFiltrados.includes(elemento)) {
+                    elemento.style.order = orden;
+                    orden++;
+                } else {
+                    elemento.style.order = ''; 
+                }
+            });
+    
+            elementosBoton.forEach(function (elemento) {
+                if (elementosFiltrados.length === 0 || elementosFiltrados.includes(elemento)) {
+                    elemento.style.display = 'block';
+                } else {
+                    elemento.style.display = 'none';
+                }
+            });
+    
+            var tieneElementosVisibles = Array.from(elementosBoton).some(function (elemento) {
+                return elemento.style.display !== 'none';
+            });
+    
+            if (contenedor.textContent.toLowerCase().includes(searchTerm) && tieneElementosVisibles) {
+                contenedor.style.display = 'flex'; 
+            } else {
+                contenedor.style.display = 'none';
+            }
+        });
+    });
+    
